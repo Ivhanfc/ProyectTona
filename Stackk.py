512bd61d2226b4862a867996f5efc44170be218a
Ivhanfc <ivhanfch1@gmail.com>
2026-06-08 12:30:04 -0700

    first version 01

---
+++ Stackk.py
@@ -0,0 +1,27 @@
+
+class Stackk:
+    def __init__(self) -> None:
+        self.Stack = []
+        self.Top = -1
+        
+    def push(self, value):
+        self.Stack.append(value)
+    
+    def pop(self):
+        self.Stack.remove (len(self.Stack) - 1)
+
+    def peek(self):
+        print(len(self.Stack[self.Top]))
+
+    def isEmpty(self):
+        if(len(self.Stack) < 0):
+            print("This fucking array is a CEROU")
+            return True
+        else: 
+            print("This ArrayStack is FULL BRODA")
+            return False
+        
+
+    def showStack(self):
+        print(self.Stack)
+
