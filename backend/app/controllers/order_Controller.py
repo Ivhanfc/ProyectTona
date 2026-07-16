ad26fa203830466332059c1130cf48e56e1390e4
Ivhanfc <ivhanfch1@gmail.com>
2026-07-16 14:26:34 -0700

    Implement methods of controller order

--- backend/app/controllers/order_Controller.py
+++ backend/app/controllers/order_Controller.py
@@ -1,26 +1,44 @@
 from sqlmodel import Session, select
+from backend.app.models.order_model import OrderModel
+from backend.app.models.user_model import UserModel
 from backend.app.models.driver_model import DriverModel
 
-class DriverController:
+class OrderController:
     
-    def create_new_driver(self, db: Session, driver_data: DriverModel) -> DriverModel | None: # this method is responsible for creating a new driver in the database. It takes a database session and driver data as input, and returns the created driver or None if the driver already exists.
+    def create_new_order_with_relations(self, db: Session, order_data: OrderModel) -> OrderModel | None: # this method is responsible for creating a new order in the database. It takes a database session and order data as input, and returns the created order or None if the order already exists.
         try:
-            statement = select(DriverModel).where(
-                (DriverModel.email == driver_data.email) | (DriverModel.username == driver_data.username)
-            )
-            existing_user = db.exec(statement).first()
-
-            if existing_user: ## if a driver with same email or username exists, returns none
+            existing_user = db.get(UserModel, order_data.user_id)
+            if not existing_user:
+                print(f"User with id {order_data.user_id} does not exist.")
                 return None
-            db.add(driver_data)
-            db.commit()
-            db.refresh(driver_data)
+           
+            existing_driver = db.get(DriverModel, order_data.driver_id)
+            if not existing_driver:
+                print(f"Driver with id {order_data.driver_id} does not exist.")
+                return None
 
-            return driver_data
-        
+            db.add(order_data)
+            db.commit()
+            db.refresh(order_data)
+            return order_data
+                
         except Exception as e:
             db.rollback()
-            print(f"Error in create Driver {e}")
+            print(f"Error in create order: {e}")
             return None
         
-        
+    def delete_order(self, db: Session, order_id: int) -> bool:
+        try:
+            order = db.get(OrderModel, order_id)
+
+            if not order:
+                print(f"Error: The order ID {order_id} not exist")
+                return False
+
+            db.delete(order)
+            db.commit()
+            return True
+        except Exception as e:
+            db.rollback()
+            print(f"Error in delete order: {e}")
+            return False 
